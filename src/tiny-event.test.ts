0365781960c752268d74fe9f9da141a3138ec9a0
Konrad Markus <konker@iki.fi>
2023-10-19 20:24:23 +0300

    Initial commit

---
+++ src/tiny-event.test.ts
@@ -0,0 +1,49 @@
+import * as P from '@konker.dev/effect-ts-prelude';
+
+import * as unit from './tiny-event';
+
+describe('TinyEvent', () => {
+  const EventTypeFoo = 'Foo';
+  const EventTypeBar = 'Bar';
+  const EventTypeBaz = 'Baz';
+  type EventType = typeof EventTypeFoo | typeof EventTypeBar | typeof EventTypeBaz;
+
+  it('should work as expected', async () => {
+    const l1 = jest.fn((_t: EventType, _d?: string) => P.Effect.unit);
+    const l2 = jest.fn((_t: EventType, _d?: string) => P.Effect.unit);
+    const l3 = jest.fn((_t: EventType, _d?: string) => P.Effect.unit);
+
+    const prog = P.pipe(
+      unit.createTinyEventDispatcher<EventType, string>(),
+      (x) => x,
+      P.Effect.flatMap(unit.addListener<EventType, string>(EventTypeFoo, l1)),
+      P.Effect.flatMap(unit.addListener<EventType, string>(EventTypeBar, l2)),
+      P.Effect.flatMap(unit.addStarListener<EventType, string>(l3)),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeFoo, 'Hello Foo 1')),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeBar, 'Hello Bar 1')),
+      P.Effect.flatMap(unit.removeListener<EventType, string>(EventTypeFoo, l1)),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeFoo, 'Hello Foo 2')),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeBar, 'Hello Bar 2')),
+      P.Effect.flatMap(unit.removeStarListener<EventType, string>(l3)),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeFoo, 'Hello Foo 3')),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeBar, 'Hello Bar 3')),
+      P.Effect.flatMap(unit.removeAllListeners<EventType, string>()),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeFoo, 'Hello Foo 4')),
+      P.Effect.flatMap(unit.notify<EventType, string>(EventTypeBar, 'Hello Bar 4'))
+    );
+
+    const result = await P.Effect.runPromise(prog);
+    await expect(result).toHaveProperty('listeners');
+    expect(l1).toHaveBeenCalledTimes(1);
+    expect(l1.mock.calls[0]).toEqual([EventTypeFoo, 'Hello Foo 1']);
+    expect(l2).toHaveBeenCalledTimes(3);
+    expect(l2.mock.calls[0]).toEqual([EventTypeBar, 'Hello Bar 1']);
+    expect(l2.mock.calls[1]).toEqual([EventTypeBar, 'Hello Bar 2']);
+    expect(l2.mock.calls[2]).toEqual([EventTypeBar, 'Hello Bar 3']);
+    expect(l3).toHaveBeenCalledTimes(4);
+    expect(l3.mock.calls[0]).toEqual([EventTypeFoo, 'Hello Foo 1']);
+    expect(l3.mock.calls[1]).toEqual([EventTypeBar, 'Hello Bar 1']);
+    expect(l3.mock.calls[2]).toEqual([EventTypeFoo, 'Hello Foo 2']);
+    expect(l3.mock.calls[3]).toEqual([EventTypeBar, 'Hello Bar 2']);
+  });
+});
---
+++ src/tiny-event.ts
@@ -0,0 +1,77 @@
+/* eslint-disable fp/no-unused-expression */
+import * as P from '@konker.dev/effect-ts-prelude';
+
+//[FIXME: should Effect<R, E, A> have R as a param?]
+//[FIXME: should Effect<R, E, A> have E as a param?]
+//[FIXME: change to succeed/tap?]
+
+export type TinyEventListener<T, X> = (eventType: T, eventData?: X) => P.Effect.Effect<never, Error, void>;
+
+export type TinyEventDispatcher<T, X> = {
+  readonly listeners: Map<T, Set<TinyEventListener<T, X>>>;
+  readonly starListeners: Set<TinyEventListener<T, X>>;
+};
+
+export function createTinyEventDispatcher<T, A>(): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> {
+  const listeners = new Map<T, Set<TinyEventListener<T, A>>>();
+  const starListeners = new Set<TinyEventListener<T, A>>();
+
+  return P.Effect.succeed({ listeners, starListeners });
+}
+
+export const addListener =
+  <T, A>(eventType: T, listener: TinyEventListener<T, A>) =>
+  (dispatcher: TinyEventDispatcher<T, A>): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> => {
+    if (!dispatcher.listeners.has(eventType)) {
+      dispatcher.listeners.set(eventType, new Set<TinyEventListener<T, A>>());
+    }
+    dispatcher.listeners.get(eventType)!.add(listener);
+    return P.Effect.succeed(dispatcher);
+  };
+
+export const addStarListener =
+  <T, A>(listener: TinyEventListener<T, A>) =>
+  (dispatcher: TinyEventDispatcher<T, A>): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> => {
+    dispatcher.starListeners.add(listener);
+    return P.Effect.succeed(dispatcher);
+  };
+
+export const removeListener =
+  <T, A>(eventType: T, listener: TinyEventListener<T, A>) =>
+  (dispatcher: TinyEventDispatcher<T, A>): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> => {
+    if (dispatcher.listeners.has(eventType)) {
+      dispatcher.listeners.get(eventType)!.delete(listener);
+    }
+    return P.Effect.succeed(dispatcher);
+  };
+
+export const removeStarListener =
+  <T, A>(listener: TinyEventListener<T, A>) =>
+  (dispatcher: TinyEventDispatcher<T, A>): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> => {
+    if (dispatcher.starListeners.has(listener)) {
+      dispatcher.starListeners.delete(listener);
+    }
+    return P.Effect.succeed(dispatcher);
+  };
+
+export const removeAllListeners =
+  <T, A>() =>
+  (_dispatcher: TinyEventDispatcher<T, A>): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> => {
+    return P.Effect.succeed({
+      listeners: new Map<T, Set<TinyEventListener<T, A>>>(),
+      starListeners: new Set<TinyEventListener<T, A>>(),
+    });
+  };
+
+export const notify =
+  <T, A>(eventType: T, eventData?: A) =>
+  (dispatcher: TinyEventDispatcher<T, A>): P.Effect.Effect<never, Error, TinyEventDispatcher<T, A>> => {
+    const listenerValues = dispatcher.listeners.get(eventType)?.values() ?? [];
+    const starListenerValues = dispatcher.starListeners.values();
+
+    return P.pipe(
+      [...listenerValues, ...starListenerValues].map((f) => f(eventType, eventData)),
+      P.Effect.all,
+      P.Effect.map((_) => dispatcher)
+    );
+  };
